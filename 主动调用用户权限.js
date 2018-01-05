2171010d76ae40ed5a2b08fc4d4234ecfe4b332c
cx091 <648571738@qq.com>
2018-01-05 15:11:58 +0800

    主动调用用户权限
    主动调用用户权限

--- 主动调用用户权限.js
+++ 主动调用用户权限.js
@@ -1,12 +1,3 @@
-# WeChat-smallProgram
-##小程序不主动获取用户权限问题
-####问题：在的有微信版本和机型中可能会不自己获取权限导致小程序的一些请求发生错误
-
-
-----------
-解：（注意在实际运行中app.js和index.js是同时运行的，应该根据项目的实际情况确定验证的位置）
-
-```
 wx.getSetting({
         success(res) {
           if (!res.authSetting['scope.userInfo', "scope.userLocation"]) {
@@ -38,4 +29,3 @@ wx.getSetting({
           }
         }
       })
-```
